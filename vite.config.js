4a0bacc460e20919d08927aa13e8b7aa4f2fd9f7
Kevin Marinucci <102044480+KevinWorkPortfolio@users.noreply.github.com>
2026-06-18 19:34:17 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,10 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         chisiamo: resolve(__dirname, 'chi-siamo.html'),
         giochi: resolve(__dirname, 'giochi.html'),
-        carte: resolve(__dirname, 'carte.html'),
+        cardmarketshop: resolve(__dirname, 'cardmarketshop.html'),
         eventi: resolve(__dirname, 'eventi.html'),
+        eventi2: resolve(__dirname, 'eventi2.html'),
+        paginavuota: resolve(__dirname, 'paginavuota.html'),
       }
     }
   }
